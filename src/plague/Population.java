2410cbab3c269269baf7494020fe522f98876dce
Fabien BERNIER <hundersoft@hotmail.fr>
2019-10-20 02:38:08 +0200

    Méthodes de base de Population.java

--- src/plague/Population.java
+++ src/plague/Population.java
@@ -3,5 +3,17 @@ package plague;
 public abstract class Population {
 
     private int count;
+    
+    public Population(int count) {
+    	this.count = count;
+    }
+    
+    public int getCount() {
+    	return this.count;
+    }
+    
+    public void setCount(int count) {
+    	this.count = count;
+    }
 
 }
