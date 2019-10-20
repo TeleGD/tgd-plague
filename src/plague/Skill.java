59485cfa9f097e8afe33ed1a1438dbee8a726c41
Frédéric Venier <frederic.venier@telecomnancy.net>
2019-10-20 02:13:30 +0200

    loading skills from json

--- src/plague/Skill.java
+++ src/plague/Skill.java
@@ -1,9 +1,56 @@
 package plague;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Skill {
+	
+	private int id;
+	
+    private List<Integer> successors;
+    
+    private int isolement;
+    private int persuasion;
+    private int cohesion;
+    
+    private int experienceNeeded;
+    
+    private String name;
+    private String description;
+    
+    public Skill() {
+    	successors = new ArrayList<>();
+    }
 
-    private List<Skill> skills;
-
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public void setIsolement(int isolement) {
+		this.isolement = isolement;
+	}
+	
+	public void setCohesion(int cohesion) {
+		this.cohesion  = cohesion;
+	}
+	
+	public void setPersuasion(int persuasion) {
+		this.persuasion = persuasion;
+	}
+	
+	public void setExperienceNeeded(int exp) {
+		this.experienceNeeded = exp;
+	}
+	
+	public void addSuccessor(int id) {
+		this.successors.add(new Integer(id));
+	}
 }
