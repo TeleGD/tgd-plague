c440f5877b8f65002a7d9830359fe36840e09db2
joannes-guichon <55346667+joannes-guichon@users.noreply.github.com>
2019-12-08 05:46:32 +0100

    ajout d'une icone pour l'accès aux compétences (à finir)

--- src/plague/Interface.java
+++ src/plague/Interface.java
@@ -5,6 +5,7 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.Image;
 
 public class Interface {
 
@@ -30,7 +31,14 @@ public class Interface {
 		//Affichage box de gauche
 		context.drawString("Ici l'expérience",40,hauteur-105);
 		context.drawString("Ici le bouton et accès aux compétences", 40, hauteur-55);
+		//Image church = new Image("/images/icons/churchico");
+		//church.draw(120,hauteur-65);
+		
 		//Affichage box centrale
+		context.setColor(Color.blue);
+			//Affichage visuel d'une barre de propagation qui affiche le détail (TO DO) du pays cliqué, et si aucun pays affiche la stat du monde
+		context.fillRect(largeur/2-100, hauteur-57, 300, 25);
+		context.setColor(Color.white);
 		context.drawString("Ici la barre de propag",largeur/2-50,hauteur-55);
 	}
 	
